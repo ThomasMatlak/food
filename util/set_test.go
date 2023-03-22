a8b2d171c43c03c23fe422ed196be8ca11d24ade
Thomas Matlak <thomas.matlak@gmail.com>
2023-03-21 21:41:38 -0400

    use table-driven subtests for Set https://go.dev/blog/subtests

--- util/set_test.go
+++ util/set_test.go
@@ -1,223 +1,168 @@
 package util // not using util_test because I don't want none to be exposed outside the package
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// TODO DRY
 // TODO test with different sized inputs
-func TestUnionOverlapping(t *testing.T) {
-	expectedUnion := Set[int]{1: none{}, 2: none{}, 3: none{}, 4: none{}}
-	s1 := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	s2 := Set[int]{2: none{}, 3: none{}, 4: none{}}
+// TODO test with one set being a subset of the other
 
-	union := Union(s1, s2)
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatalf("union does not match expected values")
+type setOperationTestCase struct {
+	name     string
+	s1       Set[int]
+	s2       Set[int]
+	expected Set[int]
+}
+
+func TestUnion(t *testing.T) {
+	testCases := []setOperationTestCase{
+		{
+			name:     "overlapping sets",
+			s1:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			s2:       Set[int]{2: none{}, 3: none{}, 4: none{}},
+			expected: Set[int]{1: none{}, 2: none{}, 3: none{}, 4: none{}},
+		},
+		{
+			name:     "same sets",
+			s1:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			s2:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			expected: Set[int]{1: none{}, 2: none{}, 3: none{}},
+		},
+		{
+			name:     "disjoint sets",
+			s1:       Set[int]{1: none{}, 2: none{}},
+			s2:       Set[int]{3: none{}, 4: none{}},
+			expected: Set[int]{1: none{}, 2: none{}, 3: none{}, 4: none{}},
+		},
+		{
+			name:     "one empty set",
+			s1:       Set[int]{1: none{}, 2: none{}},
+			s2:       Set[int]{},
+			expected: Set[int]{1: none{}, 2: none{}},
+		},
+		{
+			name:     "both empty sets",
+			s1:       Set[int]{},
+			s2:       Set[int]{},
+			expected: Set[int]{},
+		},
 	}
 
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatalf("union does not match expected values")
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := Union(tc.s1, tc.s2)
+			assert.Equal(t, actual, tc.expected)
+			// union is commutative
+			actual = Union(tc.s2, tc.s1)
+			assert.Equal(t, actual, tc.expected)
+		})
 	}
 }
 
-func TestUnionSame(t *testing.T) {
-	expectedUnion := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	s1 := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	s2 := Set[int]{1: none{}, 2: none{}, 3: none{}}
-
-	union := Union(s1, s2)
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatalf("union does not match expected values")
+func TestIntersection(t *testing.T) {
+	testCases := []setOperationTestCase{
+		{
+			name:     "overlapping sets",
+			s1:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			s2:       Set[int]{2: none{}, 3: none{}, 4: none{}},
+			expected: Set[int]{2: none{}, 3: none{}},
+		},
+		{
+			name:     "same sets",
+			s1:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			s2:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			expected: Set[int]{1: none{}, 2: none{}, 3: none{}},
+		},
+		{
+			name:     "disjoint sets",
+			s1:       Set[int]{1: none{}, 2: none{}},
+			s2:       Set[int]{3: none{}, 4: none{}},
+			expected: Set[int]{},
+		},
+		{
+			name:     "one empty set",
+			s1:       Set[int]{1: none{}, 2: none{}},
+			s2:       Set[int]{},
+			expected: Set[int]{},
+		},
+		{
+			name:     "both empty sets",
+			s1:       Set[int]{},
+			s2:       Set[int]{},
+			expected: Set[int]{},
+		},
 	}
 
-	union = Union(s2, s1)
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatalf("union does not match expected values")
-	}
-}
-
-func TestUnionDisjoint(t *testing.T) {
-	expectedUnion := Set[int]{1: none{}, 2: none{}, 3: none{}, 4: none{}}
-	s1 := Set[int]{1: none{}, 2: none{}}
-	s2 := Set[int]{3: none{}, 4: none{}}
-
-	union := Union(s1, s2)
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatalf("union does not match expected values")
-	}
-
-	union = Union(s2, s1)
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatalf("union does not match expected values")
-	}
-}
-
-func TestUnionOneEmpty(t *testing.T) {
-	expectedUnion := Set[int]{1: none{}, 2: none{}}
-	s1 := Set[int]{1: none{}, 2: none{}}
-	s2 := Set[int]{}
-
-	union := Union(s1, s2)
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatalf("union does not match expected values")
-	}
-
-	union = Union(s2, s1)
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatalf("union does not match expected values")
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := Intersection(tc.s1, tc.s2)
+			assert.Equal(t, actual, tc.expected)
+			// intersection is commutative
+			actual = Intersection(tc.s2, tc.s1)
+			assert.Equal(t, actual, tc.expected)
+		})
 	}
 }
 
-func TestUnionBothEmpty(t *testing.T) {
-	s1 := Set[int]{}
-	s2 := Set[int]{}
-	union := Union(s1, s2)
-
-	if len(union) != 0 {
-		t.Fatalf("union of empty sets has size > 0")
-	}
-}
-
-func TestIntersectionOverlapping(t *testing.T) {
-	expectedIntersection := Set[int]{2: none{}, 3: none{}}
-	s1 := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	s2 := Set[int]{2: none{}, 3: none{}, 4: none{}}
-
-	intersection := Intersection(s1, s2)
-	if !reflect.DeepEqual(intersection, expectedIntersection) {
-		t.Fatalf("intersection does not match expected values")
-	}
-
-	intersection = Intersection(s2, s1)
-	if !reflect.DeepEqual(intersection, expectedIntersection) {
-		t.Fatalf("intersection does not match expected values")
-	}
-}
-
-func TestIntersectionSame(t *testing.T) {
-	expectedIntersection := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	s1 := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	s2 := Set[int]{1: none{}, 2: none{}, 3: none{}}
-
-	intersection := Intersection(s1, s2)
-	if !reflect.DeepEqual(intersection, expectedIntersection) {
-		t.Fatalf("intersection does not match expected values")
-	}
-
-	intersection = Intersection(s2, s1)
-	if !reflect.DeepEqual(intersection, expectedIntersection) {
-		t.Fatalf("intersection does not match expected values")
-	}
-}
-
-func TestIntersectionDisjoint(t *testing.T) {
-	s1 := Set[int]{1: none{}, 2: none{}}
-	s2 := Set[int]{3: none{}, 4: none{}}
-
-	intersection := Intersection(s1, s2)
-	if len(intersection) != 0 {
-		t.Fatalf("intersection on disjoint sets has size > 0")
-	}
-
-	intersection = Intersection(s2, s1)
-	if len(intersection) != 0 {
-		t.Fatalf("intersection on disjoint sets has size > 0")
-	}
-}
-
-func TestIntersectiononeEmpty(t *testing.T) {
-	s1 := Set[int]{1: none{}, 2: none{}}
-	s2 := Set[int]{}
-	intersection := Intersection(s1, s2)
-	if len(intersection) != 0 {
-		t.Fatalf("intersection with 1 empty set has size > 0")
+func TestDifference(t *testing.T) {
+	testCases := []setOperationTestCase{
+		{
+			name:     "overlapping sets",
+			s1:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			s2:       Set[int]{2: none{}, 3: none{}, 4: none{}},
+			expected: Set[int]{1: none{}},
+		},
+		{
+			name:     "overlapping sets (reversed)",
+			s1:       Set[int]{2: none{}, 3: none{}, 4: none{}},
+			s2:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			expected: Set[int]{4: none{}},
+		},
+		{
+			name:     "same sets",
+			s1:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			s2:       Set[int]{1: none{}, 2: none{}, 3: none{}},
+			expected: Set[int]{},
+		},
+		{
+			name:     "disjoint sets",
+			s1:       Set[int]{1: none{}, 2: none{}},
+			s2:       Set[int]{3: none{}, 4: none{}},
+			expected: Set[int]{1: none{}, 2: none{}},
+		},
+		{
+			name:     "disjoint sets (reversed)",
+			s1:       Set[int]{3: none{}, 4: none{}},
+			s2:       Set[int]{1: none{}, 2: none{}},
+			expected: Set[int]{3: none{}, 4: none{}},
+		},
+		{
+			name:     "one empty set",
+			s1:       Set[int]{1: none{}, 2: none{}},
+			s2:       Set[int]{},
+			expected: Set[int]{1: none{}, 2: none{}},
+		},
+		{
+			name:     "one empty set(reversed)",
+			s1:       Set[int]{},
+			s2:       Set[int]{1: none{}, 2: none{}},
+			expected: Set[int]{},
+		},
+		{
+			name:     "both empty sets",
+			s1:       Set[int]{},
+			s2:       Set[int]{},
+			expected: Set[int]{},
+		},
 	}
 
-	intersection = Intersection(s2, s1)
-	if len(intersection) != 0 {
-		t.Fatalf("intersection with 1 empty set has size > 0")
-	}
-}
-
-func TestIntersectionBothEmpty(t *testing.T) {
-	s1 := Set[int]{}
-	s2 := Set[int]{}
-	intersection := Intersection(s1, s2)
-
-	if len(intersection) != 0 {
-		t.Fatalf("intersection on empty sets has size > 0")
-	}
-}
-
-func TestDifferenceOverlapping(t *testing.T) {
-	s1 := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	s2 := Set[int]{2: none{}, 3: none{}, 4: none{}}
-
-	difference := Difference(s1, s2)
-	expectedDifference := Set[int]{1: none{}}
-	if !reflect.DeepEqual(difference, expectedDifference) {
-		t.Fatalf("difference does not match expected values")
-	}
-
-	difference = Difference(s2, s1)
-	expectedDifference = Set[int]{4: none{}}
-	if !reflect.DeepEqual(difference, expectedDifference) {
-		t.Fatalf("difference does not match expected values")
-	}
-}
-
-func TestDifferenceSame(t *testing.T) {
-	s1 := Set[int]{1: none{}, 2: none{}}
-	s2 := Set[int]{1: none{}, 2: none{}}
-
-	difference := Difference(s1, s2)
-	if len(difference) != 0 {
-		t.Fatalf("difference of identical sets is not empty")
-	}
-}
-
-func TestDifferenceDisjoint(t *testing.T) {
-	s1 := Set[int]{1: none{}, 2: none{}}
-	s2 := Set[int]{3: none{}, 4: none{}}
-
-	difference := Difference(s1, s2)
-	expectedDifference := Set[int]{1: none{}, 2: none{}}
-	if !reflect.DeepEqual(difference, expectedDifference) {
-		t.Fatalf("difference does not match expected values")
-	}
-
-	difference = Difference(s2, s1)
-	expectedDifference = Set[int]{3: none{}, 4: none{}}
-	if !reflect.DeepEqual(difference, expectedDifference) {
-		t.Fatalf("difference does not match expected values")
-	}
-}
-
-func TestDifferenceOneEmpty(t *testing.T) {
-	s1 := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	s2 := Set[int]{}
-
-	difference := Difference(s1, s2)
-	expectedDifference := Set[int]{1: none{}, 2: none{}, 3: none{}}
-	if !reflect.DeepEqual(difference, expectedDifference) {
-		t.Fatalf("difference does not match expected values")
-	}
-
-	difference = Difference(s2, s1)
-	expectedDifference = Set[int]{}
-	if !reflect.DeepEqual(difference, expectedDifference) {
-		t.Fatalf("difference does not match expected values")
-	}
-}
-
-func TestDifferenceBothEmpty(t *testing.T) {
-	s1 := Set[int]{}
-	s2 := Set[int]{}
-
-	difference := Difference(s1, s2)
-	if len(difference) != 0 {
-		t.Fatalf("difference on empty sets has size > 0")
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := Difference(tc.s1, tc.s2)
+			assert.Equal(t, actual, tc.expected)
+			// difference is not commutative
+		})
 	}
 }
